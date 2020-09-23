4c2ace08685b3b03a8e0c13a467a8d659a4eec2f
Galaxy147 <2897044280@qq.com>
2020-09-23 18:36:05 +0800

    20200923

---
+++ target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+C:\Users\Administrator\.vscode\Code\hello-rust\target\debug\hello-rust.exe: C:\Users\Administrator\.vscode\Code\hello-rust\src\main.rs
---
+++ target/rls/debug/deps/hello_rust-0c6aab4a7872df36.d
@@ -0,0 +1,5 @@
+c:\Users\Administrator\.vscode\Code\hello-rust\target\rls\debug\deps\hello_rust-0c6aab4a7872df36.rmeta: src\main.rs
+
+c:\Users\Administrator\.vscode\Code\hello-rust\target\rls\debug\deps\hello_rust-0c6aab4a7872df36.d: src\main.rs
+
+src\main.rs:
